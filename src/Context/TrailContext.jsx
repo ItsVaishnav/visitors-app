e468d3f64e51688fbeb367b608bcfe74227e0041
ItsVaishnav <vaishnavghadgepc99@gmail.com>
2025-08-17 09:43:10 +0530

    Contaxt Fixed

--- src/Context/TrailContext.jsx
+++ src/Context/TrailContext.jsx
@@ -4,7 +4,6 @@ const TrailContext = createContext();
 
 export function TrailProvider({ children }) {
   const [data, setData] = useState("");
-
   return (
     <TrailContext.Provider value={{ data, setData }}>
       {children}
